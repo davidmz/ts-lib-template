cac0ece23c2e51f319fde8b26798773713f12b80
David Mzareulyan <david@hiero.ru>
2024-10-20 12:00:57 +0300

    Use 'extends' in eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,22 @@
 // @ts-check
-
 import eslint from '@eslint/js';
 import eslintConfigPrettier from 'eslint-config-prettier';
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
-  eslint.configs.recommended,
-  ...tseslint.configs.strict,
-  ...tseslint.configs.stylistic,
+  {
+    files: ['**/*.{ts,tsx}'],
+    extends: [
+      eslint.configs.recommended,
+      ...tseslint.configs.strict,
+      ...tseslint.configs.stylistic,
+    ],
+  },
+  {
+    files: ['**/*.{js,mjs,cjs,jsx}'],
+    extends: [tseslint.configs.disableTypeChecked],
+  },
   eslintConfigPrettier,
   eslintPluginPrettierRecommended,
   { ignores: ['build/'] },
