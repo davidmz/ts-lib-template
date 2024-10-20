46489520bb3b07acd0fec51971331f0dc849a518
David Mzareulyan <david@hiero.ru>
2024-10-20 12:33:36 +0300

    Use additiona linting rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,6 +5,7 @@ import { fileURLToPath } from 'node:url';
 import eslint from '@eslint/js';
 import eslintConfigPrettier from 'eslint-config-prettier';
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
 import tsEslint from 'typescript-eslint';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
@@ -17,16 +18,27 @@ export default tsEslint.config(
       ...tsEslint.configs.strictTypeChecked,
       ...tsEslint.configs.stylisticTypeChecked,
     ],
+    plugins: { 'simple-import-sort': simpleImportSort },
     languageOptions: {
       parserOptions: {
         project: true,
         tsconfigRootDir: __dirname,
       },
     },
+    rules: {
+      '@typescript-eslint/consistent-type-imports': 'error',
+      'simple-import-sort/imports': 'error',
+      'simple-import-sort/exports': 'error',
+    },
   },
   {
     files: ['**/*.{js,mjs,cjs,jsx}'],
     extends: [tsEslint.configs.disableTypeChecked],
+    plugins: { 'simple-import-sort': simpleImportSort },
+    rules: {
+      'simple-import-sort/imports': 'error',
+      'simple-import-sort/exports': 'error',
+    },
   },
   eslintConfigPrettier,
   eslintPluginPrettierRecommended,
